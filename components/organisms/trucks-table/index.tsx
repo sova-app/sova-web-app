b7519da9479a045992b0718e2f23b2b80877f3ea
bl1nkker <nurovich14@gmail.com>
2024-10-15 17:41:27 +0500

    style: added styles to location button

--- components/organisms/trucks-table/index.tsx
+++ components/organisms/trucks-table/index.tsx
@@ -25,10 +25,10 @@ export function TrucksTable() {
             <div className="flex items-center justify-between mb-2">
               <span className="font-semibold">{truck.name}</span>
               <Link
-                className={`inline-block px-2 py-1 text-xs font-semibold rounded-full`}
+                className={`inline-flex items-center justify-center rounded-md bg-gray-900 px-2 py-1 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300`}
                 href={`/dashboard/trucks/${truck.name}`}
               >
-                Get Started
+                Open Location
               </Link>
             </div>
             <p className="text-sm text-gray-600">{truck.name}</p>
