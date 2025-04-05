d2d0188ee556c15f6371033df9d588a86dc72e40
bl1nkker <nurovich14@gmail.com>
2025-04-06 01:37:10 +0500

    feat: added checkbox, command and popover components

---
+++ components/ui/command.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Command as CommandPrimitive } from "cmdk";
+import { cn } from "@/lib/utils";
+
+const Command = CommandPrimitive;
+
+const CommandGroup = CommandPrimitive.Group;
+
+const CommandItem = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive.Item
+    ref={ref}
+    className={cn(
+      "relative flex w-full cursor-default select-none items-center rounded-md py-2 px-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground",
+      className
+    )}
+    {...props}
+  />
+));
+CommandItem.displayName = CommandPrimitive.Item.displayName;
+
+export { Command, CommandGroup, CommandItem };
